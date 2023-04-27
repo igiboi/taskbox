a3d879210b6db43001c1e64163f570eef1148de6
Luigi Matias <36388765+igiboi@users.noreply.github.com>
2023-04-27 00:45:01 -0400

    added task list

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,15 @@
 module.exports = {
   stories: ["../src/components/**/*.stories.js"],
   staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
   features: {
-    interactionsDebugger: true,
+    interactionsDebugger: true
   },
+  docs: {
+    autodocs: true
+  }
 };
